4c373e518b0ba9db07aa842e9856bb27155e349a
wataru kikuchi <kikuchi@fms-l45.fujisan.local>
2017-04-11 16:05:42 +0900

    Fix typo

--- lib/yoto/takemitsu/enumerable.rb
+++ lib/yoto/takemitsu/enumerable.rb
@@ -2,7 +2,7 @@ module Yoto
   module Takemitsu
     module Enumerable
       ::Enumerable.module_eval do
-        def uniq_merge(keys = [], valus = [])
+        def uniq_merge(keys = [], values = [])
           self.group_by { |i| keys.map { |key| i[key] } }
               .map { |k, v|
                 v[1..-1].each { |x| values.each { |y| v[0][y] += x[y] } }
