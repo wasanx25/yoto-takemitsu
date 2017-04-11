e0dffb5bd16235917c61d6f41ccb5d8dedb10db7
wataru kikuchi <kikuchi@fms-l057.fujisan.local>
2017-04-11 11:03:11 +0900

    first commit

---
+++ lib/yoto/takemitsu.rb
@@ -0,0 +1,7 @@
+require "yoto/takemitsu/version"
+
+module Yoto
+  module Takemitsu
+    # Your code goes here...
+  end
+end
---
+++ lib/yoto/takemitsu/version.rb
@@ -0,0 +1,5 @@
+module Yoto
+  module Takemitsu
+    VERSION = "0.1.0"
+  end
+end
